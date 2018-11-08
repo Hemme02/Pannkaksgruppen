55d725574c250ce3fe46af50dffa3d170a8f4ce7
lars <lars.onselius@telia.com>
2018-11-08 23:28:43 +0100

    Added class for questions.

---
+++ src/QuestionClass.java
@@ -0,0 +1,12 @@
+public class QuestionClass {
+    String question;
+    String[] choices;
+    int answerIndex;
+
+    public QuestionClass(String question, String[] choices, int answerIndex) {
+        this.question = question;
+        this.choices = choices;
+        this.answerIndex = answerIndex;
+    }
+
+}
